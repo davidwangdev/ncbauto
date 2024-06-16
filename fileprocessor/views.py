67ba59b57559bac58cf200cf1fe923d610616934
David Wang <dwang21@wgu.edu>
2024-06-16 02:10:37 -0500

    Added functionality for breast cases

--- fileprocessor/views.py
+++ fileprocessor/views.py
@@ -126,27 +126,62 @@ def handle_surgeries(file):
     else:
         raise ValueError("Unsupported file format. Only CSV and XLS/XLSX are supported.")
 
+    # Removes duplicate event IDs in column EVENT_ID
     df_unique = df.drop_duplicates(subset=['EVENT_ID'])
 
+    # Array of surgeon names
+    # Might need to change depending on how surgeon names are profiled
     surgeon_names = [
         "BURGESS MD, MEGAN", "CYR MD, STEVEN", "DEBERARDINO MD, THOMAS", 
         "FERGUSON MD, EARL", "GARCIA MD, FRANCISCO", "KAISER MD, BRYAN", 
         "KREINES DO, ALEXANDER", "LYNCH MD, JAMIE", "NILSSON MD, JOEL", 
-        "SWANN MD, MATTHEW", "VIROSLAV MD, SERGIO", "DANIEL MD, DAVID"
+        "SWANN MD, MATTHEW", "VIROSLAV MD, SERGIO"
     ]
 
+    # Surgeons that require 4 bags per case
     multiply_by_4 = ["DEBERARDINO", "KREINES", "NILSSON", "LYNCH"]
 
-    # Count occurrences of each surgeon in dataframe
     surgeon_count = {}
-    CABG_count = df_unique['PROC_TEXT'].str.contains('CABG', case=False, na=False).sum()
-    TAVR_count = df_unique['PROC_TEXT'].str.contains('TAVR', case=False, na=False).sum()
 
     for surgeon_name in surgeon_names:
-        last_name = surgeon_name.split(' ')[0]
-        count = df_unique[df_unique['SURGEON'].str.contains(surgeon_name, na=False)].shape[0]
-        total_bags = count * 4 if last_name in multiply_by_4 else count
+        last_name = surgeon_name.split(' ')[0] # Get last name of surgeon
+        count = df_unique[df_unique['SURGEON'].str.contains(surgeon_name, na=False)].shape[0] # Count number of occurrences of each surgeon
+        total_bags = count * 4 if last_name in multiply_by_4 else count # Calculate total bags for each surgeon
         surgeon_count[last_name] = (count, total_bags)
+
+    # Array of breast surgeons
+    breast_surgeons = [
+        "CORNEJO MD", "GASSMAN MD, ANDREW", "OCHOA MD, OSCAR", "WHIPPLE MD, LAUREN"
+    ]
+
+    breast_count = {surgeon.split(" ")[0]: 0 for surgeon in breast_surgeons}
+
+    # Filter the DataFrame for breast surgeons
+    breast_df = df[df['SURGEON'].apply(lambda x: any(name in x for name in breast_surgeons))]
+
+    # Dictionary to track seen EVENT_IDs
+    seen_id = set()
+
+    # Iterate over the filtered DataFrame
+    for index, row in breast_df.iterrows():
+        event_id = row['EVENT_ID']
+        surgeon = row['SURGEON']
+        proc_text = row['PROC_TEXT']
+        
+        # Check if EVENT_ID has already been seen
+        if event_id in seen_id:
+            continue
+        
+        # Check if "BREAST" is in the PROC_TEXT
+        if "BREAST" in proc_text.upper():
+            last_name = surgeon.split(" ")[0]
+            breast_count[last_name] += 1
+            seen_id.add(event_id)
+
+
+    # Count CABG and TAVR cases for the week
+    CABG_count = df_unique['PROC_TEXT'].str.contains('CABG', case=False, na=False).sum()
+    TAVR_count = df_unique['PROC_TEXT'].str.contains('TAVR', case=False, na=False).sum()
 
     # Write results to Excel file
     wb = Workbook()
@@ -171,10 +206,6 @@ def handle_surgeries(file):
             ws.append([last_name, original_count, total_bags, 'Tumescent Solution – NS 1000ml + TXA 1gm + Lidocaine 1% 50ml + Epi 1ml. Profile but wait for request to mix due to low stability.'])
             continue
         if(last_name == "CYR"):
-            original_count, total_bags = count
-            ws.append([last_name, original_count, total_bags, 'Heparin 30,000 unit in 1000ml NS for Cell Saver.'])
-            continue
-        if(last_name == "DANIEL" ):
             original_count, total_bags = count
             ws.append([last_name, original_count, total_bags, 'Heparin 30,000 unit in 1000ml NS for Cell Saver.'])
             continue
@@ -217,6 +248,28 @@ def handle_surgeries(file):
         
         original_count, total_bags = count
         ws.append([last_name, original_count, total_bags])
+
+    ws.append([])
+
+    ws.append(["Surgeon Last Name", "Breast Cases Count", "Total Bags", "Bag Name"])
+
+    header_row2 = ws[15]
+    for cell in header_row2:
+        cell.fill = header_fill
+        cell.font = font
+
+    for surgeon, count in breast_count.items():
+        total_bags = count
+        if(surgeon == "CORNEJO"):
+            ws.append([surgeon, count, total_bags, "Tumescent Solution – LR 1000ml + Lidocaine 1% 50ml + Epi 1ml: Profile but wait for request to mix due to low stability."])
+        if(surgeon == "GASSMAN"):
+            ws.append([surgeon, count, total_bags, "PRMA solutions: Papaverine 120mg in NS 100ml and Heparin 20,000 unit in NS 500mL."])
+        if(surgeon == "OCHOA"):
+            ws.append([surgeon, count, total_bags, "PRMA solutions: Papaverine 120mg in NS 100ml and Heparin 20,000 unit in NS 500mL."])
+        if(surgeon == "WHIPPLE"):
+            ws.append([surgeon, count, total_bags, "PRMA solutions: Papaverine 120mg in NS 100ml and Heparin 20,000 unit in NS 500mL."])
+
+    ws.append([])
 
     ws.append(["CABG Cases This Week: ", CABG_count])
     ws.append(["TAVR Cases This Week: ", TAVR_count])
