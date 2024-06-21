dad5d100fe6e296c9c84f3f5597f34e3b869db36
David Wang <dwang21@wgu.edu>
2024-06-21 02:56:56 -0500

    Added static files

--- NCB/settings.py
+++ NCB/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
